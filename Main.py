a95097c379dc6e802f3ab29183477aef189d8387
unknown <matthiaspudel@gmail.com>
2021-12-04 23:07:35 +0100

    Model data

--- Main.py
+++ Main.py
@@ -6,31 +6,30 @@ Created on Sat Nov 20 14:37:04 2021
 """
 
 import pandas as pd
-import numpy as np
-import sys
 import warnings
+from GetData import *
 warnings.filterwarnings("ignore")
-
 
 # ===========================================
 # Empirical Evidence - Parameter and Settings
 # ===========================================
-from DataPreparation import *
 start_date = pd.Timestamp('2000-01-01')
 end_date = pd.Timestamp('2019-12-31')
-adj_preprocess = True
+from DataPreparation import *
+adj_preprocess = False
 adj_uw_matching = False
 adj_time_range = False
 adj_close_price = False
 
 from FeatureEngineering import *
-adj_feat_eng = True
-adj_public_features = True
-ew_rets = True
-period_length = 15
+adj_feat_eng = False
+adj_public_feat = False
+index_weight = 'Equal'
+port_days = 15
+scale_factor = 100
 # ===========================================
 # Prediction Model - Parameter and Settings
-# ===========================================
+# ===========================================F
 
 if adj_preprocess == True:
     prep_obj = DataPreparation(start_date, end_date)
@@ -43,13 +42,18 @@ else:
     prep_obj = get_object(output_path, prep_obj_file)
     
 if adj_feat_eng == True:
-    feat_eng_obj = FeatureEngineering(prep_obj)
+    feat_eng_obj = FeatureEngineering(prep_obj, scale_factor)
     feat_eng_obj.preprocessing()
     feat_eng_obj.firm_features()
-    feat_eng_obj.public_features(ew_rets, period_length, adj_public_features)
+    feat_eng_obj.public_features(index_weight, port_days, adj_public_feat)
+    feat_eng_obj.private_features()
     save_obj(feat_eng_obj, output_path, feat_eng_obj_file)
     full_data = feat_eng_obj.full_data
+    model_data = feat_eng_obj.model_data
 else:
     feat_eng_obj = get_object(output_path, feat_eng_obj_file)
     full_data = feat_eng_obj.full_data
+    model_data = feat_eng_obj.model_data
+    
+x = model_data.isna().sum()
     
