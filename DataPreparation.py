e67459ceef79915abedf3c949ae88ed683824090
unknown <matthiaspudel@gmail.com>
2021-12-05 23:03:45 +0100

    Tested Update

--- DataPreparation.py
+++ DataPreparation.py
@@ -18,74 +18,55 @@ class DataPreparation:
     def __init__(self, start_date, end_date):
         self.start_date = start_date
         self.end_date = end_date
+        self.start_year = start_date.strftime('%Y')
+        self.end_year = end_date.strftime('%Y')
         
-    def rough_preprocessing(self):
+    def rough_preprocessing(self, adj_time_range):
         sdc_raw = pd.read_csv(input_path+'\\'+ sdc_raw_file,
                               parse_dates = ['IssueDate', 'FilingDate'])
         
         dup_ident = ['Issuer']
-        mask = (sdc_raw['IssueDate'] >= self.start_date) &\
-                (sdc_raw['IssueDate'] <= self.end_date) &\
-                (sdc_raw['FilingDate'] <= self.end_date) &\
-                (pd.isnull(sdc_raw['IssueDate']) == False) &\
-                (pd.isnull(sdc_raw['FilingDate']) == False) &\
-                (sdc_raw['IPO'] == 'Yes') &\
-                (sdc_raw['ADR'] == 'No') &\
-                (sdc_raw['CEF'] == 'No') &\
-                (sdc_raw['Units'] == 'No') &\
-                (pd.isnull(sdc_raw['REIT']) == True) &\
-                (pd.isnull(sdc_raw['CUSIP9']) == False) &\
-                (sdc_raw['OfferPrice'] >= 5)
+        sdc = sdc_raw.drop_duplicates(subset = dup_ident)
+        ext_port_dt = self.start_date - DateOffset(years=1)
         
-        sdc = sdc_raw.loc[mask]
-        sdc = sdc.drop_duplicates(subset = dup_ident)
-        self.sdc = sdc
-# =========================  
-        ipo_port_start = self.start_date - DateOffset(years=1)
-        mask_ipo_port = (sdc_raw['IssueDate'] >= ipo_port_start) &\
-                        (sdc_raw['IssueDate'] <= self.end_date) &\
-                        (sdc_raw['FilingDate'] <= self.end_date) &\
-                        (pd.isnull(sdc_raw['IssueDate']) == False) &\
-                        (pd.isnull(sdc_raw['FilingDate']) == False) &\
-                        (sdc_raw['IPO'] == 'Yes') &\
-                        (sdc_raw['ADR'] == 'No') &\
-                        (sdc_raw['CEF'] == 'No') &\
-                        (sdc_raw['Units'] == 'No') &\
-                        (pd.isna(sdc_raw['REIT']) == True) &\
-                        (pd.isna(sdc_raw['CUSIP9']) == False) &\
-                        (sdc_raw['OfferPrice'] >= 5)
-                        
-        ipo_port_data = sdc_raw.loc[mask_ipo_port]
-        ipo_port_data = ipo_port_data.drop_duplicates(subset = dup_ident)
-        self.ipo_port_data = ipo_port_data
-        
-    def build_aux_vars(self, adj_time_range):
-        start_year = self.start_date.strftime('%Y')
+        rough_filter = (sdc['IssueDate'] >= ext_port_dt) &\
+                       (sdc['IssueDate'] <= self.end_date) &\
+                       (sdc['FilingDate'] <= self.end_date) &\
+                       (pd.isnull(sdc['IssueDate']) == False) &\
+                       (pd.isnull(sdc['FilingDate']) == False) &\
+                       (pd.isnull(sdc['CUSIP9']) == False)       
+        sdc = sdc.loc[rough_filter]
+
+        start_year = ext_port_dt.strftime('%Y')
         end_year = self.end_date.strftime('%Y')
-        ncusip_quotes = self.sdc['CUSIP9'].str[:8]
-        ncusip_port = self.ipo_port_data['CUSIP9'].str[:8]
-        ncusip_quote_file = f'NCUSIP_{start_year}_{end_year}_Quotes.txt'
-        ncusip_port_file = f'NCUSIP_{start_year}_{end_year}_PortfolioReturns.txt'
-        self.sdc['NCUSIP'] = ncusip_quotes
-        self.ipo_port_data['NCUSIP'] = ncusip_port
-        self.start_year = start_year
-        self.end_year = end_year
-  
+        ncusip = sdc['CUSIP9'].str[:8]        
+        ncusip_file = f'NCUSIP_{start_year}_{end_year}.txt'
+        sdc['NCUSIP'] = ncusip       
         if adj_time_range == True:
-            ncusip_quotes.to_csv(output_path+'\\'+ncusip_quote_file,
-                                 header = False,
-                                 index = False)
-            
-            ncusip_port.to_csv(output_path+'\\'+ncusip_port_file,
-                               header = False,
-                               index = False)
+            ncusip.to_csv(output_path+'\\'+ncusip_file,
+                          header = False,
+                          index = False)
             
             sys.exit(exit_message)
-# =========================        
+# =========================
+        ext_filter = (sdc['IPO'] == 'Yes') &\
+                     (sdc['ADR'] == 'No') &\
+                     (sdc['CEF'] == 'No') &\
+                     (sdc['Units'] == 'No') &\
+                     (pd.isnull(sdc['REIT']) == True) &\
+                     (sdc['OfferPrice'] >= 5)
+                     
+        port_data = sdc.loc[ext_filter]
+        base_filter = port_data['IssueDate']>=self.start_date
+        base = port_data.loc[base_filter]
+        self.port_data = port_data
+        self.base = base
+        
+    def build_aux_vars(self):   
         onebday_offset = pd.offsets.BusinessDay(1)
         twobday_offset = pd.offsets.BusinessDay(2)
-        first_trade_dt = self.sdc['IssueDate'] + onebday_offset
-        second_trade_dt = self.sdc['IssueDate'] + twobday_offset
+        first_trade_dt = self.base['IssueDate'] + onebday_offset
+        second_trade_dt = self.base['IssueDate'] + twobday_offset
         
         last_trade_wk_dt = pd.Series([])
         for index, value in first_trade_dt.items():
@@ -97,52 +78,52 @@ class DataPreparation:
                 last_bday = last_bday + pd.Timedelta('7 days')
             last_trade_wk_dt.loc[index] = last_bday
             
-        self.sdc['FirstTradeDate'] = first_trade_dt
-        self.sdc['SecondTradeDate'] = second_trade_dt
-        self.sdc['LastTradeDateWK'] = last_trade_wk_dt
+        self.base['FirstTradeDate'] = first_trade_dt
+        self.base['SecondTradeDate'] = second_trade_dt
+        self.base['LastTradeDateWK'] = last_trade_wk_dt
 # =========================        
-        cpi_merge_dt = self.sdc['IssueDate'].dt.strftime('%Y-%m')
-        self.sdc['CPI_MergeDate'] = cpi_merge_dt
+        cpi_merge_dt = self.base['IssueDate'].dt.strftime('%Y-%m')
+        self.base['CPI_MergeDate'] = cpi_merge_dt
 # =========================        
-        registration_days = (self.sdc['IssueDate'] - self.sdc['FilingDate'])
+        registration_days = (self.base['IssueDate'] - self.base['FilingDate'])
         registration_days = registration_days.dt.days
-        self.sdc['RegistrationDays'] = registration_days
+        self.base['RegistrationDays'] = registration_days
         
     def extended_preprocessing(self, adj_uw_matching):
-        exchange = self.sdc['ExchangeWhereIssuWillBeLi']
+        exchange = self.base['ExchangeWhereIssuWillBeLi']
         exchange_dummies = pd.get_dummies(exchange)
         exchange_dummies = exchange_dummies.astype(float)
         exchange_cols = ['AMEX', 'NASDQ', 'NYSE']
         
-        self.sdc = pd.concat([self.sdc, 
+        self.base = pd.concat([self.base, 
                               exchange_dummies[exchange_cols]], 
                              axis = 1)
 # =========================        
-        mean_prc_rg = np.where(pd.isnull(self.sdc['OriginalMiddleOfFilingPriceRange']) == True,
-                                          self.sdc['AmendedMiddleOfFilingPrice'],
-                                          self.sdc['OriginalMiddleOfFilingPriceRange'])
+        mean_prc_rg = np.where(pd.isnull(self.base['OriginalMiddleOfFilingPriceRange']) == True,
+                                         self.base['AmendedMiddleOfFilingPrice'],
+                                         self.base['OriginalMiddleOfFilingPriceRange'])
                                 
-        min_prc_rg = np.where(pd.isnull(self.sdc['OriginalLowFilingPrice']) == True,
-                                          self.sdc['AmendedLowFilingPrice'],
-                                          self.sdc['OriginalLowFilingPrice'])
+        min_prc_rg = np.where(pd.isnull(self.base['OriginalLowFilingPrice']) == True,
+                                        self.base['AmendedLowFilingPrice'],
+                                        self.base['OriginalLowFilingPrice'])
                                 
-        max_prc_rg = np.where(pd.isnull(self.sdc['OriginalHighFilingPrice']) == True,
-                                          self.sdc['AmendedHighFilingPrice'],
-                                          self.sdc['OriginalHighFilingPrice'])
+        max_prc_rg = np.where(pd.isnull(self.base['OriginalHighFilingPrice']) == True,
+                                        self.base['AmendedHighFilingPrice'],
+                                        self.base['OriginalHighFilingPrice'])
         
-        self.sdc['MeanPriceRange'] = mean_prc_rg
-        self.sdc['MinPriceRange'] = min_prc_rg
-        self.sdc['MaxPriceRange'] = max_prc_rg
+        self.base['MeanPriceRange'] = mean_prc_rg
+        self.base['MinPriceRange'] = min_prc_rg
+        self.base['MaxPriceRange'] = max_prc_rg
 # =========================         
-        shares_filed = np.where(pd.isnull(self.sdc['SharesFiledSumOfAllMkts']) == True,
-                                          self.sdc['AmendedShsFiledSumOfAllMkts'],
-                                          self.sdc['SharesFiledSumOfAllMkts'])
+        shares_filed = np.where(pd.isnull(self.base['SharesFiledSumOfAllMkts']) == True,
+                                          self.base['AmendedShsFiledSumOfAllMkts'],
+                                          self.base['SharesFiledSumOfAllMkts'])
         
-        shares_filed_idx = self.sdc['SharesFiledSumOfAllMkts'].index
+        shares_filed_idx = self.base['SharesFiledSumOfAllMkts'].index
         shares_filed = pd.Series(shares_filed, index = shares_filed_idx)
         shares_filed = shares_filed.str.replace(',', '')
         shares_filed = shares_filed.astype(float)
-        self.sdc['SharesFiled'] = shares_filed
+        self.base['SharesFiled'] = shares_filed
 # =========================         
         ritter_data = pd.read_excel(input_path+'\\'+uw_file,
                                             engine = 'openpyxl',
@@ -172,11 +153,18 @@ class DataPreparation:
         
         self.rank_data = rank_data[rank_data_cols]
 
-        sdc_uwriter = self.sdc['LeadManagersLongName']
+        cols = ['DealNumber', 'LeadManagersLongName']
+        sdc_uwriter = self.base[cols]
         match_results = pd.DataFrame()
         if adj_uw_matching == True:
-            for index, value in sdc_uwriter.items():
-                print(index)
+            iter_count = 0
+            progress = 100
+            print('Underwriter matching started')
+            for index, row in sdc_uwriter.iterrows():
+                iter_count += 1
+                if iter_count % progress == 0:
+                    print(f'Progress: {iter_count} items')
+                value = row['LeadManagersLongName']
                 char_pos = value.find('|')
                 if char_pos != -1:
                     sdc_name = value[:char_pos]
@@ -189,22 +177,29 @@ class DataPreparation:
                 
                 matching = process.extractOne(sdc_name, 
                                               rank_data['Name'])
-            
+                
+                match_results.loc[index, 'DealNumber'] = row['DealNumber']
                 match_results.loc[index, 'SDC_Name'] = sdc_name
                 match_results.loc[index, 'Ritter_Name'] = matching[0]
                 match_results.loc[index, 'Matching_Level'] = matching[1]
-                
-            match_results.to_csv(output_path+'\\'+uw_matching_file)
-        else:
-            index_col = ['Unnamed: 0']
-            match_results = pd.read_csv(output_path+'\\'+uw_matching_file,
-                                        index_col = index_col)
             
-        self.sdc = self.sdc.join(match_results, how = 'left')
+            file_name = uw_matching_file
+            file_name = f'{self.start_year}_{self.end_year}_{file_name}'
+            match_results.to_csv(output_path+'\\'+file_name, index = False)
+            print('Underwriter matching finished', '\n')
+        else:
+            file_name = uw_matching_file
+            file_name = f'{self.start_year}_{self.end_year}_{file_name}'
+            match_results = pd.read_csv(output_path+'\\'+file_name)
+            
+        self.base = pd.merge(self.base,
+                             match_results, 
+                             how = 'left',
+                             on = 'DealNumber')
         
     def data_merging(self, adj_close_price):
         total_assets = pd.read_csv(input_path+'\\'+total_assets_file)
-        self.full_data = pd.merge(self.sdc,
+        self.full_data = pd.merge(self.base,
                                   total_assets,
                                   how = 'left',
                                   on = 'DealNumber')
