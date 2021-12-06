fc3f0b9560803700fde3a71e39d9805e2b0208af
unknown <matthiaspudel@gmail.com>
2021-12-06 23:35:52 +0100

    FamaMacBeth Regression

--- Regression.py
+++ Regression.py
@@ -28,8 +28,6 @@ class Regression:
         exog_var.remove('InitialReturn')
         self.endog_var = endog_var
         self.exog_var = exog_var
-        
-        print('Test')
 
     def ols_regression(self):
         return
@@ -45,8 +43,65 @@ class Regression:
         period = period.strftime('%Y')
         period = pd.Series(period)
         
+        fmb_coefs = pd.DataFrame()
+        fmb_pvalues = pd.DataFrame()
+        fmb_sub_key = pd.DataFrame()
+        fmb_avg_results = {}
+        sub_key_cols = ['RSquared']
         for index, value in period.iteritems():
-            print('Test')
+            year_filter = self.base['IssueYear'] == value
+            reg_data = self.base[year_filter]
+            endog = reg_data[self.endog_var]
+            exog = reg_data[self.exog_var]
+            exog = sm.add_constant(exog)
+            
+            fmb_reg = sm.OLS(endog = endog, exog = exog)
+            fmb_reg = fmb_reg.fit(cov_type='HC0')
+            
+            coef = fmb_reg.params
+            coef.name = value
+            pvalue = fmb_reg.pvalues
+            pvalue.name = value
+            r_sqr = fmb_reg.rsquared_adj    
+            nobs = fmb_reg.nobs
+
+            fmb_coefs = pd.concat([fmb_coefs, coef], axis=1)
+            fmb_pvalues = pd.concat([fmb_pvalues, pvalue], axis=1)
+            fmb_sub_key.loc['RSquared', value] = r_sqr
+            fmb_sub_key.loc['AmountItems', value] = nobs
+            
+        avg_coefs = fmb_coefs.mean(axis = 1)
+        avg_coefs.name = 'Coefficient'
+        avg_coefs = pd.DataFrame(avg_coefs)
+        avg_pvalues = fmb_pvalues.mean(axis = 1)
+        avg_pvalues.name = 'pvalue'
+        avg_pvalues = pd.DataFrame(avg_pvalues)
+        avg_key = avg_coefs.join(avg_pvalues)
+    
+        avg_sub_key = fmb_sub_key.mean(axis = 1)
+        avg_sub_key = avg_sub_key.loc[sub_key_cols]
+        avg_sub_key.name = 'Value'
+        
+        fmb_avg_results['KeyFigures'] = avg_key
+        fmb_avg_results['SubKeyFigures'] = avg_sub_key
+        
+        self.fmb_coefs = fmb_coefs
+        self.fmb_pvalues = fmb_pvalues
+        self.fmb_avg_results = fmb_avg_results
+        
+        
+    def build_results(self):
+        # x = avg_results.copy()
+        # x.loc['SharesFiled', 'Coefficient'] = 12
+        # x.loc['SharesFiled', 'pvalue'] = 14
+        # y = avg_results.join(x, how = 'right', lsuffix=' ', rsuffix=' ')
+        return
+        
+        
+    
+        
+        
+
             
             
         
